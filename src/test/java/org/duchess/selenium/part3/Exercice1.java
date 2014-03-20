cd546cca52b226ccdd69950bc7c0b6541574c595
Mathilde Lemee <mathilde.lemee@yahoo.fr>
2014-03-20 22:03:36 +0100

    partie 3

---
+++ src/test/java/org/duchess/selenium/part3/Exercice1.java
@@ -0,0 +1,60 @@
+package org.duchess.selenium.part3;
+
+import org.duchess.selenium.Util;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Objectif : Faire passer les tests
+ * Ne pas passer plus de 5 min sur le deuxieme test.
+ */
+public class Exercice1 {
+
+    private WebDriver driver;
+
+    @Before
+    public void beforeTest() {
+        driver = new FirefoxDriver();
+        driver.get(Util.getPortfolio());
+
+    }
+
+    @Test
+      public void when_click_to_next_then_new_datas_with_implicit_wait() throws InterruptedException {
+          assertThat(driver.findElements(By.cssSelector("#containment-portfolio")).size()).isEqualTo(1);
+          driver.findElement(By.ByLinkText.linkText("WEB")).click();
+
+          assertThat(driver.findElements(By.cssSelector(".audio.hidden")).size()).isEqualTo(1);
+      }
+
+    @Test
+    //Appelez nous quand vous voyez le probleme
+    public void when_click_to_next_then_new_datas_with_implicit_wait_2() throws InterruptedException {
+        assertThat(driver.findElements(By.cssSelector("#containment-portfolio")).size()).isEqualTo(1);
+        driver.findElement(By.ByLinkText.linkText("WEB")).click();
+
+        assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(4);
+    }
+
+
+
+    @Test
+    public void when_click_to_next_then_new_datas_with_explicit_wait() throws InterruptedException {
+        assertThat(driver.findElements(By.cssSelector("#containment-portfolio")).size()).isEqualTo(1);
+        driver.findElement(By.ByLinkText.linkText("WEB")).click();
+
+        assertThat(driver.findElements(By.cssSelector("#containment-portfolio li:not(.hidden)")).size()).isEqualTo(4);
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+
+}
